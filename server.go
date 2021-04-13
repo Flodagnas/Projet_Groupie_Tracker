e87b241fe9f21ed5d8e77ca4fea8fd1240308f0e
NatSch45 <nathan.schneider4505@gmail.com>
2021-04-13 08:51:46 +0200

    First architecture

--- server.go
+++ server.go
@@ -9,18 +9,56 @@ var t *template.Template
 
 func main() {
 	t = template.Must(template.ParseFiles("templates/index.html"))
-	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	// Importation du CSS :
+	http.Handle("/static/css/", http.StripPrefix("/static/css/", http.FileServer(http.Dir("static/css"))))
 	http.Handle("/", http.NotFoundHandler())
+
+	// Accès aux pages :
 	http.HandleFunc("/home", home)
 	http.Handle("/home/", http.NotFoundHandler())
+	http.HandleFunc("/artists", artists)
+	http.Handle("/artists/", http.NotFoundHandler())
+	http.HandleFunc("/dates", dates)
+	http.Handle("/dates/", http.NotFoundHandler())
+	http.HandleFunc("/locations", locations)
+	http.Handle("/locations/", http.NotFoundHandler())
+
+	// Port :
 	http.ListenAndServe(":8000", nil)
 }
 
 func home(w http.ResponseWriter, req *http.Request) {
 	tHome, err := template.ParseFiles("templates/home.html")
-	if (err != nil) {
-		w.WriteHeader(400)	
+	if err != nil {
+		w.WriteHeader(400)
 	}
-	
+
 	tHome.Execute(w, nil)
+}
+
+func artists(w http.ResponseWriter, req *http.Request) {
+	tArtists, err := template.ParseFiles("templates/artists.html")
+	if err != nil {
+		w.WriteHeader(400)
+	}
+
+	tArtists.Execute(w, nil)
+}
+
+func dates(w http.ResponseWriter, req *http.Request) {
+	tDates, err := template.ParseFiles("templates/dates.html")
+	if err != nil {
+		w.WriteHeader(400)
+	}
+
+	tDates.Execute(w, nil)
+}
+
+func locations(w http.ResponseWriter, req *http.Request) {
+	tLocations, err := template.ParseFiles("templates/locations.html")
+	if err != nil {
+		w.WriteHeader(400)
+	}
+
+	tLocations.Execute(w, nil)
 }
