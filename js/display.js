03cc1e4b5e49ade85251de63dcd53a9efee91ba8
NatSch45 <nathan.schneider4505@gmail.com>
2021-04-18 18:02:22 +0200

    Affichage des données par carte

--- js/display.js
+++ js/display.js
@@ -8,11 +8,40 @@ function loadData(artists) {
     }
 }
 
+// function makeCard(artist) {
+//     let div = document.createElement('div')
+//     div.className = "card"
+//     let p = document.createElement('p')
+//     p.innerText += artist.name
+//     div.appendChild(p)
+//     document.querySelector('.cards').appendChild(div)
+// }
+
 function makeCard(artist) {
-    let div = document.createElement('div')
-    div.className = "card"
-    let p = document.createElement('p')
-    p.innerText += artist.name
-    div.appendChild(p)
-    document.querySelector('.cards').appendChild(div)
+    let card = document.createElement('div')
+    card.className = 'card'
+    let url = "url('" + artist.image + "')"
+    card.style.background = url
+    card.style.backgroundRepeat = "no-repeat"
+    card.style.backgroundSize = "contain"
+    card.style.backgroundPosition = "0"
+    card.setAttribute("onclick", "this.classList.toggle('expanded'); this.querySelector('.text').classList.toggle('displayed')")
+    let text = document.createElement('div')
+    text.className = 'text'
+    let textContent = document.createElement('div')
+    textContent.className = 'text-content'
+    let title = document.createElement('h1')
+    title.innerText += artist.name
+    title.className = 'title'
+    let bodyText = document.createElement('div')
+    bodyText.className = 'body-text'
+
+    card.appendChild(text)
+    text.appendChild(textContent)
+    textContent.appendChild(title)
+    textContent.appendChild(bodyText)
+    card.innerHTML += `<svg class="chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 35" width="30"><path d="M5 30L50 5l45 25" fill="none" stroke="#000" stroke-width="5"/></svg>`
+
+    document.querySelector('.testStyle').appendChild(card)
+
 }
