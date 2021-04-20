9e76e830910a571f3ebac064f96786267935d78d
NatSch45 <nathan.schneider4505@gmail.com>
2021-04-20 14:33:22 +0200

    Fusion display --> server

---
+++ js/display.js
@@ -0,0 +1,64 @@
+console.log("Script display loaded")
+
+function loadData(artists) {
+    artists = JSON.parse(artists)
+
+    for (artist of artists) {
+        makeCard(artist)
+    }
+}
+
+function makeCard(artist) {
+    let members
+    if (artist.members.length == 1) {
+        let p = document.createElement('p')
+        p.innerText += artist.members[0]
+        members = p
+    } else {
+        let list = document.createElement('ul')
+        for (member of artist.members) {
+            let line = document.createElement('li')
+            line.innerText += member
+            list.appendChild(line)
+        }
+        members = list
+    }
+    members.classList.add('members')
+
+    let card = document.createElement('div')
+    card.className = 'card'
+    let url = "url('" + artist.image + "')"
+    card.style.background = url
+    card.style.backgroundRepeat = "no-repeat"
+    card.style.backgroundSize = "contain"
+    card.style.backgroundPosition = "0 20%"
+    card.setAttribute("onclick", "cardOnClick(this)")
+    let text = document.createElement('div')
+    text.className = 'text'
+    let textContent = document.createElement('div')
+    textContent.className = 'text-content'
+    let title = document.createElement('h1')
+    title.innerText += artist.name
+    title.className = 'title'
+    let bodyText = document.createElement('div')
+    bodyText.className = 'body-text'
+
+    card.appendChild(text)
+    text.appendChild(textContent)
+    textContent.appendChild(title)
+    textContent.appendChild(bodyText)
+    card.innerHTML += `<svg class="chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 35" width="30"><path d="M5 30L50 5l45 25" fill="none" stroke="#000" stroke-width="5"/></svg>`
+    document.querySelector('.cards').appendChild(card)
+
+    let svg = document.querySelectorAll('.chevron')[document.querySelectorAll('.chevron').length - 1]
+    document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].insertBefore(members, svg)
+    
+}
+
+function cardOnClick(el) {
+    el.classList.toggle('expanded')
+    el.querySelector('.text').classList.toggle('displayed')
+    el.querySelector('.members').classList.toggle('displayed')
+}
+
+loadData(responseData)
--- server.go
+++ server.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 var t *template.Template
 
+type dataStruct struct {
+	ResponseData string
+}
+
+var data dataStruct
+
 func main() {
 	t = template.Must(template.ParseFiles("templates/index.html"))
 	// Importation des fichiers statiques :
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
 	http.Handle("/", http.NotFoundHandler())
 
 	// Accès aux pages :
@@ -28,6 +37,7 @@ func main() {
 }
 
 func home(w http.ResponseWriter, req *http.Request) {
+
 	tHome, err := template.ParseFiles("templates/home.html")
 	if err != nil {
 		w.WriteHeader(400)
@@ -37,12 +47,13 @@ func home(w http.ResponseWriter, req *http.Request) {
 }
 
 func artists(w http.ResponseWriter, req *http.Request) {
+	data.ResponseData = loadAPI()
 	tArtists, err := template.ParseFiles("templates/artists.html")
 	if err != nil {
 		w.WriteHeader(400)
 	}
 
-	tArtists.Execute(w, nil)
+	tArtists.Execute(w, data)
 }
 
 func dates(w http.ResponseWriter, req *http.Request) {
@@ -61,4 +72,19 @@ func locations(w http.ResponseWriter, req *http.Request) {
 	}
 
 	tLocations.Execute(w, nil)
+}
+
+func loadAPI() string {
+	response, errGet := http.Get("https://groupietrackers.herokuapp.com/api/artists")
+
+	if errGet != nil {
+		log.Fatal(errGet)
+	}
+
+	responseData, errReadAll := ioutil.ReadAll(response.Body)
+	if errReadAll != nil {
+		log.Fatal(errReadAll)
+	}
+
+	return string(responseData)
 }
