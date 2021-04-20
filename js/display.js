958dbccab1ba13fa7e7a2975f3d250c2932e13aa
NatSch45 <nathan.schneider4505@gmail.com>
2021-04-20 10:11:42 +0200

    Pont API : serveur Go --> script JS | fonctionnel

--- js/display.js
+++ js/display.js
@@ -1,22 +1,38 @@
-fetch('https://cors-anywhere.herokuapp.com/https://groupietrackers.herokuapp.com/api/artists')
-    .then((response) => response.json()) // parse the response from JSON
-    .then(loadData) // .then will call the function with the JSON value
+console.log("Script display loaded")
 
 function loadData(artists) {
+    artists = JSON.parse(artists)
+
     for (artist of artists) {
         makeCard(artist)
     }
 }
 
 function makeCard(artist) {
+    let members
+    if (artist.members.length == 1) {
+        let p = document.createElement('p')
+        p.innerText += artist.members[0]
+        members = p
+    } else {
+        let list = document.createElement('ul')
+        for (member of artist.members) {
+            let line = document.createElement('li')
+            line.innerText += member
+            list.appendChild(line)
+        }
+        members = list
+    }
+    members.classList.add('members')
+
     let card = document.createElement('div')
     card.className = 'card'
     let url = "url('" + artist.image + "')"
     card.style.background = url
     card.style.backgroundRepeat = "no-repeat"
     card.style.backgroundSize = "contain"
-    card.style.backgroundPosition = "0"
-    card.setAttribute("onclick", "this.classList.toggle('expanded'); this.querySelector('.text').classList.toggle('displayed')")
+    card.style.backgroundPosition = "0 20%"
+    card.setAttribute("onclick", "cardOnClick(this)")
     let text = document.createElement('div')
     text.className = 'text'
     let textContent = document.createElement('div')
@@ -32,7 +48,17 @@ function makeCard(artist) {
     textContent.appendChild(title)
     textContent.appendChild(bodyText)
     card.innerHTML += `<svg class="chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 35" width="30"><path d="M5 30L50 5l45 25" fill="none" stroke="#000" stroke-width="5"/></svg>`
-
     document.querySelector('.cards').appendChild(card)
+
+    let svg = document.querySelectorAll('.chevron')[document.querySelectorAll('.chevron').length - 1]
+    document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].insertBefore(members, svg)
     
 }
+
+function cardOnClick(el) {
+    el.classList.toggle('expanded')
+    el.querySelector('.text').classList.toggle('displayed')
+    el.querySelector('.members').classList.toggle('displayed')
+}
+
+loadData(responseData)
--- server.go
+++ server.go
@@ -2,25 +2,21 @@ package main
 
 import (
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 var t *template.Template
 
-type Response struct {
-	id           int
-	image        string
-	name         string
-	members      []string
-	creationDate int
-	firstAlbum   string
-	locations    string
-	concertDates string
-	relations    string
+type dataStruct struct {
+	ResponseData string
 }
 
+var data dataStruct
+
 func main() {
-	// getJson()
+
 	t = template.Must(template.ParseFiles("templates/index.html"))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("js"))))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
@@ -31,39 +27,26 @@ func main() {
 }
 
 func home(w http.ResponseWriter, req *http.Request) {
+	data.ResponseData = loadAPI()
 	tHome, err := template.ParseFiles("templates/home.html")
 	if err != nil {
 		w.WriteHeader(400)
 	}
 
-	tHome.Execute(w, nil)
+	tHome.Execute(w, data)
 }
 
-// func getJson() {
-// 	url := "https://groupietrackers.herokuapp.com/api/artists"
+func loadAPI() string {
+	response, errGet := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 
-// 	var tab []Response
+	if errGet != nil {
+		log.Fatal(errGet)
+	}
 
-// 	res, errGet := http.Get(url)
-// 	// fmt.Println(res)
+	responseData, errReadAll := ioutil.ReadAll(response.Body)
+	if errReadAll != nil {
+		log.Fatal(errReadAll)
+	}
 
-// 	if errGet != nil {
-// 		panic(errGet.Error())
-// 	}
-
-// 	body, errRead := ioutil.ReadAll(res.Body)
-// 	// fmt.Println(body)
-
-// 	if errRead != nil {
-// 		panic(errRead.Error())
-// 	}
-
-// 	// var artist Response
-
-// 	err := json.Unmarshal(body, &tab)
-// 	fmt.Println(err)
-
-// 	fmt.Println(tab)
-// 	// fmt.Println(artist.name)
-
-// }
+	return string(responseData)
+}
