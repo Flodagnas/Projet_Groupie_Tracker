9a31759d8c646093c9d419af7113416560ee47ca
LBROCHARD <73283488+LBROCHARD@users.noreply.github.com>
2021-04-23 11:51:43 +0200

    update cards

--- js/display.js
+++ js/display.js
@@ -10,10 +10,14 @@ function loadData(artists) {
 
 function makeCard(artist) {
     let members
+    let beforeMembers = document.createElement('p')
+    beforeMembers.className = 'beforeMembers'
     if (artist.members.length == 1) {
         let p = document.createElement('p')
         p.innerText += artist.members[0]
         members = p
+        beforeMembers.innerText += "Full Name :"
+
     } else {
         let list = document.createElement('ul')
         for (member of artist.members) {
@@ -22,16 +26,35 @@ function makeCard(artist) {
             list.appendChild(line)
         }
         members = list
+        beforeMembers.innerText += "Members :"
+
     }
     members.classList.add('members')
 
     let card = document.createElement('div')
     card.className = 'card'
     let url = "url('" + artist.image + "')"
-    card.style.background = url
-    card.style.backgroundRepeat = "no-repeat"
-    card.style.backgroundSize = "contain"
-    card.style.backgroundPosition = "0 20%"
+    //card.style.background = url
+    // card.style.backgroundRepeat = "no-repeat"
+    // card.style.backgroundSize = "contain"
+    // card.style.backgroundPosition = "0 20%"
+    let image = document.createElement('img')
+    image.className = 'image'
+    image.setAttribute("src", artist.image)
+    let beforeDate = document.createElement('p')
+    beforeDate.innerText += "Creation date :"
+    beforeDate.className = 'beforeDate'
+
+    let firstAlbum = document.createElement('p')
+    firstAlbum.innerText += artist.firstAlbum
+    firstAlbum.className = 'firstAlbum'
+    let beforeAlbum = document.createElement('p')
+    beforeAlbum.innerText += "First Album :"
+    beforeAlbum.className = 'beforeAlbum'
+
+    let creationDate = document.createElement('p')
+    creationDate.innerText += artist.creationDate 
+    creationDate.className = 'creationDate'
     card.setAttribute("onclick", "cardOnClick(this)")
     let text = document.createElement('div')
     text.className = 'text'
@@ -42,16 +65,22 @@ function makeCard(artist) {
     title.className = 'title'
     let bodyText = document.createElement('div')
     bodyText.className = 'body-text'
-
     card.appendChild(text)
+    card.appendChild(image)
+    card.appendChild(beforeMembers)
+    card.appendChild(members)
     text.appendChild(textContent)
     textContent.appendChild(title)
     textContent.appendChild(bodyText)
+    card.appendChild(beforeDate)
+    card.appendChild(creationDate)
+    card.appendChild(beforeAlbum)
+    card.appendChild(firstAlbum)
     card.innerHTML += `<svg class="chevron" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 35" width="30"><path d="M5 30L50 5l45 25" fill="none" stroke="#000" stroke-width="5"/></svg>`
     document.querySelector('.cards').appendChild(card)
 
     let svg = document.querySelectorAll('.chevron')[document.querySelectorAll('.chevron').length - 1]
-    document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].insertBefore(members, svg)
+    //document.querySelectorAll('.card')[document.querySelectorAll('.card').length - 1].insertBefore(members, svg)
     
 }
 
@@ -59,6 +88,12 @@ function cardOnClick(el) {
     el.classList.toggle('expanded')
     el.querySelector('.text').classList.toggle('displayed')
     el.querySelector('.members').classList.toggle('displayed')
+    el.querySelector('.image').classList.toggle('expanded')
+    el.querySelector('.beforeMembers').classList.toggle('displayed')
+    el.querySelector('.beforeDate').classList.toggle('displayed')
+    el.querySelector('.creationDate').classList.toggle('displayed')
+    el.querySelector('.beforeAlbum').classList.toggle('displayed')
+    el.querySelector('.firstAlbum').classList.toggle('displayed')
 }
 
-loadData(responseData) 
+loadData(responseData)
