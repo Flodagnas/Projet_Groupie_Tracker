c2df6c6eeffd1737b9be49f4a59eb8dc7c997c14
root <root@MSI.localdomain>
2021-04-26 09:28:45 +0200

    search2

--- js/search.js
+++ js/search.js
@@ -1,27 +1,31 @@
-console.log("Script search loaded")
+const searchBar = document.getElementById('searchBar');
+let hpCharacters = [];
 
-let search = ""
-let searchSelect = document.getElementById('searchSelect').value
-let tabLines = []
+searchBar.addEventListener('keyup', (e) => {
+    const searchString = e.target.value.toLowerCase();
 
-// Input actualisant en temps réel les données
-document.getElementById('searchInput').addEventListener('keyup', () => {
-    tabLines = []
-    search = document.getElementById('searchInput').value
-    callJson()
-})
+    const filteredCharacters = hpCharacters.filter((character) => {
+        return (
+            character.name.toLowerCase().includes(searchString) ||
+            character.house.toLowerCase().includes(searchString)
+        );
+    });
+    displayCharacters(filteredCharacters);
+});
 
-// Liste à choix des catégories
-document.getElementById('searchSelect').addEventListener('change', () => {
-    tabLines = []
-    searchSelect = document.getElementById('searchSelect').value
-    callJson()
-})
+const displayCharacters = (characters) => {
+    const htmlString = characters
+        .map((character) => {
+            return `
+            <li class="character">
+                <h2>${character.name}</h2>
+                <p>House: ${character.house}</p>
+                <img src="${character.image}"></img>
+            </li>
+        `;
+        })
+        .join('');
+    charactersList.innerHTML = htmlString;
+};
 
-// Fonction de suppression des lignes du tableau
-function deleteLines() {
-    let lines = document.getElementsByClassName('line')
-    for (let line of Array.from(lines)) {
-        line.remove()
-    }
-}
+loadCharacters();
