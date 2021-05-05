0152385654189733a2715b547707d3b85967abf4
FDAGNAS <florian.dagnas@ynov.com>
2021-05-05 08:44:22 +0200

    fin geo sans css

--- static/js/display.js
+++ static/js/display.js
@@ -12,26 +12,46 @@ function geo(artists) {
 function APIRelation(relations) {
     console.log(relations);
     relationsGroupe = JSON.parse(relations)
-    var lastDate = []
-    let date = []
-    let locations = ""
-    let preLocations = ""
+    var lastDate = ""
+    let dates = []
+    let lastLocation = ""
+    let preLocation = ""
     let i = 0
     let preDate = ""
-    for (locations in relationsGroupe['datesLocations']){
+    /*for (locations in relationsGroupe['datesLocations']){
         date = relationsGroupe['datesLocations'][locations]
         i += 1
         if (i == Object.getOwnPropertyNames(relationsGroupe['datesLocations']).length -1) {
             preDate = date
             preLocations = locations
         }
-
-    };
-    lastDate = date[date.length - 1]
+    };*/
+    for (locations in relationsGroupe['datesLocations']){
+        relationsGroupe['datesLocations'][locations].forEach(date => {
+            dates.push(date)
+        });
+    }
+    dates.sort(function(a, b){
+        let dateA = new Date(a.split("-").reverse().join("-"))
+        let dateB = new Date(b.split("-").reverse().join("-"))
+        return dateA - dateB
+    })
+    preDate = dates[dates.length - 2]
+    lastDate = dates[dates.length - 1]
+    for (locations in relationsGroupe['datesLocations']){
+        relationsGroupe['datesLocations'][locations].forEach(date => {
+            if (date == preDate) {
+                preLocation = locations
+            }
+            if (date == lastDate) {
+                lastLocation = locations
+            }
+        });
+    }
     document.getElementById('section_text1').insertAdjacentHTML('beforeEnd',preDate);
-    document.getElementById('section_text2').insertAdjacentHTML('beforeEnd',preLocations);
+    document.getElementById('section_text2').insertAdjacentHTML('beforeEnd',preLocation);
     document.getElementById('section_text3').insertAdjacentHTML('beforeEnd',lastDate);
-    document.getElementById('section_text4').insertAdjacentHTML('beforeEnd',locations);
+    document.getElementById('section_text4').insertAdjacentHTML('beforeEnd',lastLocation);
     console.log(lastDate);
 };
 
